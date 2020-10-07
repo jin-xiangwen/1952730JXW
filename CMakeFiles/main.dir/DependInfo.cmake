cce5f9b7f0c461cae5a419e26a59c327a2267653
jin-xiangwen <249195279@qq.com>
2020-10-07 14:37:07 +0800

    FOR WORK

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jin/文档/HOMEWORK/main.cpp" "/home/jin/文档/HOMEWORK/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jin/下载/mindvision/include"
+  "/home/jin/下载/opencv-3.4.11/build"
+  "/home/jin/下载/opencv-3.4.11/include"
+  "/home/jin/下载/opencv-3.4.11/include/opencv"
+  "/home/jin/下载/opencv-3.4.11/modules/core/include"
+  "/home/jin/下载/opencv-3.4.11/modules/flann/include"
+  "/home/jin/下载/opencv-3.4.11/modules/imgproc/include"
+  "/home/jin/下载/opencv-3.4.11/modules/ml/include"
+  "/home/jin/下载/opencv-3.4.11/modules/photo/include"
+  "/home/jin/下载/opencv-3.4.11/modules/video/include"
+  "/home/jin/下载/opencv-3.4.11/modules/dnn/include"
+  "/home/jin/下载/opencv-3.4.11/modules/features2d/include"
+  "/home/jin/下载/opencv-3.4.11/modules/imgcodecs/include"
+  "/home/jin/下载/opencv-3.4.11/modules/shape/include"
+  "/home/jin/下载/opencv-3.4.11/modules/videoio/include"
+  "/home/jin/下载/opencv-3.4.11/modules/calib3d/include"
+  "/home/jin/下载/opencv-3.4.11/modules/highgui/include"
+  "/home/jin/下载/opencv-3.4.11/modules/objdetect/include"
+  "/home/jin/下载/opencv-3.4.11/modules/stitching/include"
+  "/home/jin/下载/opencv-3.4.11/modules/superres/include"
+  "/home/jin/下载/opencv-3.4.11/modules/ts/include"
+  "/home/jin/下载/opencv-3.4.11/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
